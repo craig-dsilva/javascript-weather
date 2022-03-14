729511f927d7079123dd473a55f0f299ed48e39e
craig-dsilva <90406619+craig-dsilva@users.noreply.github.com>
2022-03-14 10:58:26 +0000

    Some changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # javascript-weather
+
+This is a weather app created using JavaScript and the data is fetched using the [OpenWeather](https://openweathermap.org) api
+
+When the user enters a location in the search, a random image is displayed using the [Unsplash](https://unsplash.com) api
--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-window.addEventListener("load", () => {
+const weather = () => {
   let apiKey = "a8f6179630de50582203e8324ec4a157";
 
   const searchButtonEl = document.querySelector(".search button");
   const searchBarEl = document.querySelector(".search-bar");
 
-  let lat;
-  let lon;
+  document.body.style.backgroundImage = 'url("img/london.jpg")';
+  // Photo by Benjamin Davies on Unsplash
 
+  // If the user's location is available this will render the data for that location
   navigator.geolocation.getCurrentPosition((position) => {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
     )
@@ -17,34 +18,41 @@ window.addEventListener("load", () => {
       .then((data) => displayWeather(data));
   });
 
+  // Users can manually search for locations
   searchButtonEl.addEventListener("click", () => {
+    document.body.style.backgroundImage = `url("https://source.unsplash.com/1600x900/?${searchBarEl.value}")`;
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${searchBarEl.value}&units=metric&appid=${apiKey}`
     )
       .then((response) => response.json())
       .then((data) => displayWeather(data));
+
+    searchBarEl.value = ""; // Clears the search input when the search button is clicked
   });
-});
+};
 
 const displayWeather = (data) => {
+  // Destructures the data/object
   const { name } = data;
   const { icon, description } = data.weather[0];
-  const { temp, humidity } = data.main;
+  const { temp, feels_like, humidity } = data.main;
   const { speed } = data.wind;
 
   const cityEl = document.querySelector(".city");
   const iconEl = document.querySelector(".icon");
   const descriptionEl = document.querySelector(".description");
   const temperatureEl = document.querySelector(".temperature");
+  const feelsLikeEl = document.querySelector(".feels-like");
   const humidityEl = document.querySelector(".humidity");
   const windEl = document.querySelector(".wind");
 
-  document.body.style.backgroundColor = `url('https://source.unsplash.com/1600x900/?"${name}"')`;
-
   cityEl.innerText = name;
-  iconEl.src = `https://openweathermap.org/img/wn/${icon}.png`;
-  descriptionEl.innerText = description;
+  iconEl.src = `https://openweathermap.org/img/wn/${icon}.png`; // Renders an icon according to the weather condition provided by the api
   temperatureEl.innerHTML = `${Math.round(temp)}&deg;C`;
+  feelsLikeEl.innerHTML = `Feels like ${Math.round(feels_like)}&deg;C`;
+  descriptionEl.innerText = description;
   humidityEl.innerText = `Humidity: ${humidity}%`;
-  windEl.innerText = `Wind speed: ${speed.toFixed(2)} km/h`;
+  windEl.innerText = `Wind speed: ${Math.round(speed * 3.6)} km/h`;
 };
+
+window.onload = weather;
